9794c998f29c462aab9f2843acae702edb3d7a68
Tom <2508893587@qq.com>
2025-07-23 15:53:04 +0800

    美化vitepress,启用backtop(返回顶部)

--- .vitepress/theme/components/backtotop.vue
+++ .vitepress/theme/components/backtotop.vue
@@ -127,7 +127,7 @@ onBeforeUnmount(() => {
 
 .progress-ring-circle {
   fill: none;
-  stroke: #af3ea6;
+  stroke: #3eaf7c;
   stroke-width: 3;
   stroke-dasharray: 264; /* 2 * π * 42 */
   stroke-linecap: round;
